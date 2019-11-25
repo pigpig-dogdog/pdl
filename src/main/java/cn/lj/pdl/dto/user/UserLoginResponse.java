f82f138c5e845c42fab6bc2fc2a8c36fc6de4cb4
luojian <luojian9508@163.com>
2019-11-25 18:23:01 +0800

    用户注册和登录接口的 http response 去掉id字段

--- src/main/java/cn/lj/pdl/dto/user/UserLoginResponse.java
+++ src/main/java/cn/lj/pdl/dto/user/UserLoginResponse.java
@@ -8,7 +8,6 @@ import lombok.Data;
  */
 @Data
 public class UserLoginResponse {
-    private Long id;
     private String username;
     private String token;
 }
--- src/main/java/cn/lj/pdl/dto/user/UserRegisterResponse.java
+++ src/main/java/cn/lj/pdl/dto/user/UserRegisterResponse.java
@@ -8,7 +8,6 @@ import lombok.Data;
  */
 @Data
 public class UserRegisterResponse {
-    private Long id;
     private String username;
     private String token;
 }
